38988b16cd9d2d1ae0b57a4fe6824b674f6e8b93
kingstonskye <125080702+kingstonskye@users.noreply.github.com>
2023-07-21 06:45:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=Hf0b_-hVBS4")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=UtV0Z-_hrU8")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=uyaKoj7wABY")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[3].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=LqsaryT1Txk")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[4].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=D1H4o4FW-wA")
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[5].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=xAmB_TJ-pyU")
+})
+
+
